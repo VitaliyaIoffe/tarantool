dd4ca4b5415362d0ce44d04eb9b91fb08a430d18
Mergen Imeev <imeevma@gmail.com>
2021-09-10 14:20:01 +0300

    sql: remove AggStep0 and OP_BuiltinFunction0
    This patch moves the initialization of sql_context out of the VDBE. This
    allows us to remove the opcodes OP_BuiltinFunction0 and OP_AggStep0,
    which work in a rather strange way. Moreover, due to the changes these
    opcodes make to the VDBEs, it is possible that the estimated size of the
    VDBE may change during execution of VDBE, which could lead to various
    problems.
    
    Needed for #4145

--- src/box/sql/vdbeaux.c
+++ src/box/sql/vdbeaux.c
@@ -1068,14 +1068,12 @@ displayP4(Op * pOp, char *zTemp, int nTemp)
 				   func->def->param_count);
 			break;
 		}
-#if defined(SQL_DEBUG) || defined(VDBE_PROFILE)
 	case P4_FUNCCTX:{
-			struct func *func = pOp->p4.func;
+			struct func *func = pOp->p4.pCtx->func;
 			sqlXPrintf(&x, "%s(%d)", func->def->name,
 				   func->def->param_count);
 			break;
 		}
-#endif
 	case P4_BOOL:
 			sqlXPrintf(&x, "%d", pOp->p4.b);
 			break;
